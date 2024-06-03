97d2da3b5096ad6c757ffa0b3e1a2114e4147fec
lakshmi753 <lakshmi753sahu@gmail.com>
2024-06-03 11:07:23 +0530

    App.js and css files were implemented.

--- profile_card/src/App.js
+++ profile_card/src/App.js
@@ -1,1 +1,86 @@
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="pic.jpg" alt="Coding 👩‍💻" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Coding Introduction 👩‍💻</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook and eat, or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skillObj={skill} key={skill.skill} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skillObj }) {
+  return (
+    <div className="skill" style={{ backgroundColor: skillObj.color }}>
+      <span>{skillObj.skill}</span>
+      <span>
+        {skillObj.level === "advanced"
+          ? "💪"
+          : skillObj.level === "intermediate"
+          ? "👍"
+          : "👶"}
+      </span>
+    </div>
+  );
+}
