ce72d5966807f4d5bcfa9ea089bc1dce11b83a7f
lakshmi753 <lakshmi753sahu@gmail.com>
2024-06-04 10:45:11 +0530

    App.js and css files were implemented.

--- daily_thought/src/App.js
+++ daily_thought/src/App.js
@@ -1,1 +1,49 @@
+import { useState } from "react";
+import "./index.css";
 
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World !!</h1>
+      <h2>🌄 Today's thought 🏜️</h2>
+      <ThoughtBox />
+    </div>
+  );
+}
+
+function ThoughtBox() {
+  const [advice, steAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  const handleAdvice = async function () {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    //console.log(data);
+    steAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  };
+
+  /* if (count === 0) {
+    handleAdvice();
+  }*/
+
+  return (
+    <div>
+      <p>{advice}</p>
+      <div>
+        <button onClick={handleAdvice} style={{ cursor: "pointer" }}>
+          Get Advice
+        </button>
+      </div>
+      <div>
+        <p>
+          You have already read{" "}
+          <span>
+            <b>{count}</b>
+          </span>{" "}
+          {count <= 1 ? "piece" : "pieces"} of advice
+        </p>
+      </div>
+    </div>
+  );
+}
