7be7475e7b545fe58e8eb18dcab4f032faf876e5
lakshmi753 <lakshmi753sahu@gmail.com>
2024-06-24 16:28:46 +0530

    html and css structuring of the project is implemented.

--- guess_my_number/src/App.js
+++ guess_my_number/src/App.js
@@ -1,1 +1,41 @@
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <LeftSection />
+        <RightSection />
+      </main>
+    </div>
+  );
+}
 
+function Header() {
+  return (
+    <header>
+      <h1>Guess My Number!</h1>
+      <p className="between">(Between 1 and 20)</p>
+      <button className="btn again">Again!</button>
+      <div className="number">?</div>
+    </header>
+  );
+}
+
+function LeftSection() {
+  return (
+    <section className="left">
+      <input type="number" className="guess" />
+      <button className="btn check">Check!</button>
+    </section>
+  );
+}
+
+function RightSection() {
+  return (
+    <section className="right">
+      <p className="message">Start Guessing...</p>
+      <p className="label-score">💯 Score : 20</p>
+      <p className="label-highscore">🥇 Highscore : 0</p>
+    </section>
+  );
+}
