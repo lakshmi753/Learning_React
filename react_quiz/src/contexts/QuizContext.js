3d024e9b7948100122d287219d87d4b35db35d37
lakshmi753 <lakshmi753sahu@gmail.com>
2024-07-17 21:05:28 +0530

    Some components like header and main are included in the app.js file.

--- react_quiz/src/contexts/QuizContext.js
+++ react_quiz/src/contexts/QuizContext.js
@@ -1,8 +1,33 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const QuizContext = createContext();
 
+const initialState = {
+  status: "loading",
+  questions: [],
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+};
+
+function reducer({ state, action }) {
+  switch (action.type) {
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      break;
+  }
+}
+
 function QuizProvider({ children }) {
+  const [{ status, questions, index, answer, points, highScore }, dispatch] =
+    useReducer(reducer, initialState);
+
   return <QuizContext.Provider value={{}}>{children}</QuizContext.Provider>;
 }
 
