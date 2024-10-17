aae5332b33db5290877d2f0e957b84a51f3b78c5
lakshmi753 <lakshmi753sahu@gmail.com>
2024-10-17 17:11:43 +0530

    Process of fetching data is done.

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="product-container">App</div>;
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function getProducts() {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`https://dummyjson.com/products`);
+
+        if (!res.ok) {
+          throw new Error("Something went wrong! Please try again.");
+        }
+
+        const data = await res.json();
+
+        if (!data) {
+          throw new Error("Products not Available!!");
+        }
+
+        console.log(data.products);
+        setProduct(data.products);
+      } catch (error) {
+        //console.error(error.message);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getProducts();
+  }, []);
+
+  return (
+    <div className="product-container">
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Error error={error} />
+      ) : (
+        product.map((item) => <Item key={item.id} itemObj={item} />)
+      )}
+    </div>
+  );
+}
+
+function Loader() {
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+function Error({ error }) {
+  return (
+    <div>
+      <p>Error : {error}</p>
+    </div>
+  );
+}
+
+function Item({ itemObj }) {
+  return (
+    <div className="item-box">
+      <img alt={itemObj.title} src={itemObj.images} />
+      <p>{itemObj.title}</p>
+    </div>
+  );
 }
 
 export default App;
