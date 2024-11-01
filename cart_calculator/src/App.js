97f73719c1517c4ce7da8b9e8abcc9be8c561d0f
lakshmi753 <lakshmi753sahu@gmail.com>
2024-11-01 20:45:37 +0530

    Cart calculator logic is implemented.

--- cart_calculator/src/App.js
+++ cart_calculator/src/App.js
@@ -2,6 +2,20 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cartProduct, setCartProduct] = useState([]);
+
+  const totalCartItems = cartProduct.length;
+  const totalCartAmount = cartProduct.reduce((acc, cur) => acc + cur.price, 0);
+
+  function handleAddCartProducts(itemObj) {
+    setCartProduct((cartProduct) => [...cartProduct, itemObj]);
+  }
+
+  function handleRemoveCartProduct(itemObj) {
+    setCartProduct((cartProduct) =>
+      cartProduct.filter((item) => item.id !== itemObj.id)
+    );
+  }
 
   useEffect(function () {
     async function getProducts() {
@@ -28,26 +42,34 @@ function App() {
     <div className="container">
       <div className="menu-box">
         {products.map((item) => (
-          <Item key={item.id} itemObj={item} />
+          <Item
+            key={item.id}
+            itemObj={item}
+            onAddCartItem={handleAddCartProducts}
+            onRemoveCartItem={handleRemoveCartProduct}
+            cartProduct={cartProduct}
+          />
         ))}
       </div>
       <div className="cartItem-box">
         <h2 className="cart-items">Cart Items</h2>
-        <h3 className="cart-item-qty">Total Quantity : X</h3>
-        <h3 className="cart-item-price">Total Price : X</h3>
-        <div className="cart-box">
-          <img alt="title" className="cart-item-img"></img>
-          <div className="cart-item-box">
-            <p className="item-name">Title</p>
-            <p className="item-price">Price</p>
+        <h3 className="cart-item-qty">Total Quantity : {totalCartItems}</h3>
+        <h3 className="cart-item-price">Total Price : {totalCartAmount}</h3>
+        {cartProduct.map((item) => (
+          <div className="cart-box" key={item.id}>
+            <img alt="title" className="cart-item-img" src={item.image}></img>
+            <div className="cart-item-box">
+              <p className="item-name">{item.title}</p>
+              <p className="item-price">{item.price}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
 }
 
-function Item({ itemObj }) {
+function Item({ itemObj, onAddCartItem, onRemoveCartItem, cartProduct }) {
   return (
     <div className="item-box">
       <img alt={itemObj.title} src={itemObj.image} className="item-img" />
@@ -57,8 +79,18 @@ function Item({ itemObj }) {
           <b>{itemObj.price}</b>
         </span>
       </div>
-      <button className="add-btn btn">Add To Cart</button>
-      <button className="remove-btn btn">Remove from cart</button>
+      {cartProduct.some((item) => item.id === itemObj.id) ? (
+        <button
+          className="remove-btn btn"
+          onClick={() => onRemoveCartItem(itemObj)}
+        >
+          Remove From Cart
+        </button>
+      ) : (
+        <button className="add-btn btn" onClick={() => onAddCartItem(itemObj)}>
+          Add To Cart
+        </button>
+      )}
     </div>
   );
 }
